9c3094f16297191f378b8699b0ad75f34d4778cf
one62 <onemo@kakao.com>
2023-05-11 20:39:27 +0900

    230511

--- BOJ/1782.cpp
+++ BOJ/1782.cpp
@@ -1,45 +1,12 @@
 #include <iostream>
 using namespace std;
 
-bool IsMirrorNum(long long num)
-{
-	string str = to_string(num);
-	for (int i = 0; i < str.length() / 2; i++)
+// 1 8 0 2 5
+int main() {
+	long long a, b;
+	cin >> a >> b;
+	for (long long i = a; i <= b; i++)
 	{
-		if (str[i] == '0' || str[i] == '1' || str[i] == '8')
-		{
-			if (str[str.length() - i - 1] != str[i])
-				return false;
-		}
-		else if (str[i] == '2')
-		{
-			if (str[str.length() - i - 1] != '5')
-				return false;
-		}
-		else if (str[i] == '5')
-		{
-			if (str[str.length() - i - 1] != '2')
-				return false;
-		}
-		else
-			return false;
+		
 	}
-	if (str.length() % 2 == 1) {
-		int mid = str.length() / 2;
-		if (str[mid] != '0' && str[mid] != '1' && str[mid] != '8') return false;
-	}
-	return true;
-}
-
-int main()
-{
-	long long A, B;
-	cin >> A >> B;
-
-	long long count = 0;
-	for (long long i = A; i <= B; i++)
-	{
-		if (IsMirrorNum(i)) count++;
-	}
-	cout << count;
 }
