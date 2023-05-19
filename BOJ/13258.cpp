9fc99a08823e72d4147a795f676a603c0a22e78a
one62 <onemo@kakao.com>
2023-05-19 18:53:58 +0900

    13258 트라이중, README 팁

--- BOJ/13258.cpp
+++ BOJ/13258.cpp
@@ -3,19 +3,21 @@
 using namespace std;
 
 int main() {
-	int N, balance, J, C;
-	cin >> N >> balance;
-	int balSum = 0;
+	int N, credit[2], J, C;
+	cin >> N >> credit[0];
+	credit[1] = 0; //credit[0] : 강호 잔고, credit[1] : 나머지 사람들 잔고 합
 	for (int i = 1; i < N; i++)
 	{
 		int t;
 		cin >> t;
-		balSum += t;
+		credit[1] += t;
 	}
 	cin >> J >> C;
+	double a = credit[0], b = credit[1];
 	for (int i = 0; i < C; i++)
 	{
-		
+		a = a + (double)J * a / (a + b);
+		b = b + (double)J * b / (a + b);
 	}
-	
+	cout << a;
 }
---
+++ README.md
@@ -0,0 +1,6 @@
+git clean : untracked 파일 삭제
+	-n : 미리보기
+	-X : gitignore만
+	-x : gitignore도
+
+파일명 변경 : F2
