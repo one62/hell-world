d293eda86e7071e7e5500fe5cda6f0e7f0314bc6
one62 <onemo@kakao.com>
2023-05-19 18:25:03 +0900

    BOJ 13258 진행중

---
+++ BOJ/13258.cpp
@@ -0,0 +1,21 @@
+//백준 13258번 복권 + 은행
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, balance, J, C;
+	cin >> N >> balance;
+	int balSum = 0;
+	for (int i = 1; i < N; i++)
+	{
+		int t;
+		cin >> t;
+		balSum += t;
+	}
+	cin >> J >> C;
+	for (int i = 0; i < C; i++)
+	{
+		
+	}
+	
+}
